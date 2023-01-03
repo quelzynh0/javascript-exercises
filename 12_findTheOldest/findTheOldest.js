b00f00e9e88d520c28d4332ec8ec21c550d8857f
Ezequiel Marinho <24194330+quelzynh0@users.noreply.github.com>
2023-01-03 15:36:59 -0300

    Resolução dos 3 testes do exercicio 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function (x) {
 
+    x.forEach(person => {
+        if (person['yearOfDeath'])
+            person['age'] = person['yearOfDeath'] - person['yearOfBirth'];
+    });
+
+    x.forEach(person => {
+        if (Number.isInteger(person['age']) == false) {
+            person['age'] = new Date().getFullYear() - person['yearOfBirth']
+        }
+        else {
+
+        }
+    });
+    let oldest = 0
+    x.forEach(person => {
+        if ((person['age']) >= oldest) {
+            oldest = person['age']
+            oldestName = person['name']
+        }
+        else {
+        }
+
+    });
+
+    return oldestName
 };
 
 // Do not edit below this line
