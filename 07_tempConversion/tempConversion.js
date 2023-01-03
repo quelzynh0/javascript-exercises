ea014f2f53f1072c9bb17dfa6ffa9a7e8da40d14
Ezequiel Marinho <24194330+quelzynh0@users.noreply.github.com>
2023-01-03 09:23:16 -0300

    Resolução dos 6 testes do exercicio 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  c = (temp - 32) / 1.8
+  c = Math.round(c * 10) / 10
+  return c
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  f = temp * 1.8 + 32
+  f = Math.round(f * 10) / 10
+  return f
 };
 
 // Do not edit below this line
