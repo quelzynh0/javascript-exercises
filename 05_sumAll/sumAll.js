1f348753d6a940f4e464bc11189d6694192af619
Ezequiel Marinho <24194330+quelzynh0@users.noreply.github.com>
2023-01-03 08:50:27 -0300

    Resolução dos 6 testes do exercicio 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,23 @@
 const sumAll = function (a, b) {
-    let sum = 0
-    if (a < b) {
-        for (a = a; a <= b; a++) {
-            sum = a + sum
+    if (a & b >= 0) {
+        let sum = 0
+        if (a < b) {
+            for (a = a; a <= b; a++) {
+                sum = a + sum
+            }
         }
+        else {
+            for (b = b; b <= a; b++) {
+                sum = b + sum
+            }
+
+        }
+        return sum
     }
     else {
-        for (b = b; b <= a; b++) {
-            sum = b + sum
-        }
-
+        return 'ERROR'
     }
-    return sum
+
 }
 
 // Do not edit below this line
