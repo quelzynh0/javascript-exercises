065d9a6521f13472b2727602076964f20fdf4982
Ezequiel Marinho <24194330+quelzynh0@users.noreply.github.com>
2023-01-03 12:19:13 -0300

    Resolução do teste do exercicio 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,19 @@
-const getTheTitles = function() {
+const getTheTitles = function () {
+    const books = [
+        {
+            title: 'Book',
+            author: 'Name'
+        },
+        {
+            title: 'Book2',
+            author: 'Name2'
+        }
+    ]
+    let titles = []
+    for (let title in books) {
+        titles.push(books[title]['title'])
+    }
+    return titles
 
 };
 
