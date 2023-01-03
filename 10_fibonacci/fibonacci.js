56b83d7b35e4d6ac8f7679dc6f99340359520cc9
Ezequiel Marinho <24194330+quelzynh0@users.noreply.github.com>
2023-01-03 11:44:49 -0300

    Resolução dos 9 testes do exercicio 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (a) {
+    if (a > 0) {
+        let fibNumber = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
+            89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765,
+            10946, 17711, 28657, 46368, 75025]
+        return fibNumber[a - 1]
+    }
+    else {
+        return 'OOPS'
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
