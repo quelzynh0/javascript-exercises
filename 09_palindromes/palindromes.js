85c0e29e6ef127927c68d5b1944b1263766ee225
Ezequiel Marinho <24194330+quelzynh0@users.noreply.github.com>
2023-01-03 11:15:46 -0300

    Resolução dos 6 testes do exercicio 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const reverseString = function (str) {
+        var splitString = str.split('');
+        var reverseArray = splitString.reverse();
+        var joinArray = reverseArray.join('');
+        return joinArray
+    };
 
+    result = reverseString(str).replace(/[.,\s\/#!$%\^&\*;:{}=\-_`~()]/g, "").toUpperCase()
+    string = str.replace(/[.,\s\/#!$%\^&\*;:{}=\-_`~()]/g, "").toUpperCase()
+    if (result === string) {
+        return true
+    }
+    else {
+        console.log(result)
+        console.log(string)
+        return false
+    }
 };
 
 // Do not edit below this line
