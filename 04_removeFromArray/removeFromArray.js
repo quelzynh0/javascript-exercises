466348af1f070180e7bdbbdf5235aae7daf49800
Ezequiel Marinho <24194330+quelzynh0@users.noreply.github.com>
2023-01-05 15:29:13 -0300

    Resolução dos 7 testes do exercicio 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function (a, b) {
-    a.splice(a.indexOf(b), 1);
-    return a
+const removeFromArray = function () {
+    let same = []
+    //Comparação e cópia dos argumentos passados com o array
+    arguments[0].forEach(string => {
+        for (let i = 1; i < arguments.length; i++) {
+            if (string == arguments[i]) {
+                same.push(arguments[i])
+            }
+            else {
+            }
+        }
+
+    })
+    //Comparação dos arrays e remoção caso seja igual
+    for (let i = 0; i < same.length; i++) {
+        index = arguments[0].indexOf(same[i]);
+        if (index > -1) {
+            arguments[0].splice(index, 1);
+        }
+    }
+    return arguments[0]
 };
 
 // Do not edit below this line
